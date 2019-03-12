75a3a2e2389bb491e843cae61249472e8d86f24d
ersin sivaz <ersinsivaz@hotmail.com>
2019-03-12 21:34:06 +0300

    On Demand Widget Rendering

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 import 'package:flutter_news/src/models/item_model.dart';
+import 'package:flutter_news/src/resources/repository.dart';
 import 'package:http/http.dart' show Client;
 
 final _root ='https://hacker-news.firebaseio.com/v0';
 
-class NewsApiProvider {
+class NewsApiProvider implements Source {
   Client client = Client();
 
   Future<List<int>> fetchTopIds() async {
--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_news/src/resources/repository.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
@@ -5,8 +6,12 @@ import 'package:path/path.dart';
 import 'dart:async';
 import '../models/item_model.dart';
 
-class NewsDbProvider {
+class NewsDbProvider implements Source,Cache {
   Database db;
+
+  NewsDbProvider(){
+    init();
+  }
 
   void init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
@@ -53,4 +58,11 @@ class NewsDbProvider {
   Future<int> addItem(ItemModel item){
     return db.insert("Items", item.toMap());
   }
+
+  @override
+  Future<List<int>> fetchTopIds() {
+    return null;
+  }
 } //cs
+
+final newsDbProvider =NewsDbProvider();
